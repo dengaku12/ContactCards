ff9c3d38c5ea18a236363c984c611c4e5227f310
Michael Odusanya <modusanya12@gmail.com>
2020-06-02 17:42:54 -0400

    Contact card complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, {Component} from "react";
+import ContactCard from "./ContactCard";
 import './App.css';
 
-class Cheese extends Component{
-  render(){
-    return <h1>I like Cheese</h1>;
-  }
-}
 class App extends Component{
   render(){
-    return <Cheese />;
+    return (
+      <>
+        <ContactCard
+          name = "Dark Pit"
+          mobile = "080232590"
+          work = "99999999969"
+          email = "electroshock12@gmail.com"
+        />
+        <ContactCard
+          name = "Young Link"
+          mobile = "82693455"
+          work = "911"
+          email = "majoras_mask420@gmail.com"
+        />
+        <ContactCard
+          name = "Lucina"
+          mobile = "44444444444"
+          work = "311"
+          email = "awakening69@gmail.com"
+        />
+      </>
+    )
   }
 }
 
